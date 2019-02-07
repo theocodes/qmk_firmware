4690becc6a82af9d17718f9011e1b88e4615272d
Theo Felippe <theocodes@gmail.com>
2019-02-07 16:12:53 +0000

    broken xd75, improved planck

--- keyboards/planck/keymaps/theocodes/keymap.c
+++ keyboards/planck/keymaps/theocodes/keymap.c
@@ -26,7 +26,8 @@ enum planck_layers {
   _LOWER,
   _RAISE,
   _PLOVER,
-  _ADJUST
+  _ADJUST,
+  _FN
 };
 
 enum planck_keycodes {
@@ -38,27 +39,47 @@ enum planck_keycodes {
   EXT_PLV
 };
 
+#define CTL_ESC MT(MOD_LCTL, KC_ESC)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
+#define F_FN LT(_FN, KC_F)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |  Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |Ctl/Esc|   A  |   S  |   D  | F_FN |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Brite | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    CTL_ESC, KC_A,    KC_S,    KC_D,    F_FN,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
     BACKLIT, KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
+
+/* FN
+ * ,-----------------------------------------------------------------------------------.
+ * |       |      |      |      |      |      |      |      |      |      |      |     |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |       |      |      |      |      |      |      |      |      |      |      |     |
+ * |------+------+------+------+------+-------+-----+------+------+------+------+------|
+ * |       |      |      |      |      |      |      |      |      |      |      |     |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |       |      |      |      |      |             |      |      |      |      |     |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_FN] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_HOME,  KC_END,  KC_INS, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET
 ),
 
 /* Colemak
