5e0f736cea781b83a15f52689e590d4c2e2bde4d
Theo Felippe <theocodes@gmail.com>
2019-02-22 10:58:26 +0000

    configured preonic

--- keyboards/preonic/keymaps/theocodes/keymap.c
+++ keyboards/preonic/keymaps/theocodes/keymap.c
@@ -55,15 +55,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|------+------+------+------+------+------|
  * |  Shift  |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |---------+------+------+------+------+------+------+------+------+------+------+------|
- * |  Ctrl   | Meh  | Alt  | GUI  |Lower |    Space    |Raise | GUI  |AltGr | Umlt | Ctrl |
+ * |  Ctrl   | Meh  | Alt  |Lower | GUI  |    Space    |Raise |      |      |      |      |
  * `--------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CTL_ESC, KC_A,    KC_S,    KC_D,    FN_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  SFT_BSP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL, MEH_GRV, KC_LALT, KC_LGUI, LOWER,   KC_SPC, KC_SPC,   RAISE,   KC_RGUI, KC_RALT, UMLAUT,  KC_RCTL  \
+  SFT_BSP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
+  KC_LCTL, MEH_GRV, KC_LALT, LOWER,   KC_LGUI, KC_SPC,  SFT_SPC, RAISE,   _______, _______, _______, _______ \
 ),
 
 /* Lower
