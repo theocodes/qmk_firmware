ccbd03bbd34147de8ea7aa9d02f3fe43f00952db
Theo Felippe <theocodes@gmail.com>
2019-04-01 21:36:52 +0100

    updates clueboard

--- keyboards/preonic/keymaps/theocodes/keymap.c
+++ keyboards/preonic/keymaps/theocodes/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   CTL_ESC, KC_A,    KC_S,    KC_D,    FN_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   SFT_BSP, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, \
-  KC_LCTL, MEH_GRV, KC_LGUI, LOWER,   KC_LALT, KC_SPC,  KC_SPC,  RAISE,   _______, _______, _______, _______ \
+  KC_LCTL, MEH_GRV, KC_LALT, LOWER,   KC_LGUI, KC_SPC,  KC_SPC,  RAISE,   _______, _______, _______, _______ \
 ),
 
 /* Lower
